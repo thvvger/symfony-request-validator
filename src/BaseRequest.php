2344c704ad6c7d827bb2e128c8e8624d27158834
Larry-Bill ADJE <thvger@gmail.com>
2024-11-16 19:32:05 +0100

    Ajout des en-têtes CORS dans validate()

--- src/BaseRequest.php
+++ src/BaseRequest.php
@@ -89,6 +89,10 @@ class BaseRequest
             'errors' => $errors
         ], Response::HTTP_BAD_REQUEST);
 
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
         $response->send();
 
         exit;
